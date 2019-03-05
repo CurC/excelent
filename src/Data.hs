bc9514042ea391138f9aceae0dcb252ce673a355
CurC <CurC@users.noreply.github.com>
2019-03-05 17:50:40 +0100

    Tried using recursion schemes

--- src/Data.hs
+++ src/Data.hs
@@ -1,31 +1,85 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Data where
 
 import qualified Data.Map as M
-import Control.Monad.State (MonadState)
+import Control.Monad.State
+import Data.Functor.Foldable (Fix(..), unfix, cata)
+import Data.Maybe
+import Debug.Trace
 
-data Expr a where
-    ConstInt :: Int -> Expr Int
-    OperPlus :: Expr Int -> Expr Int -> Expr Int
-    RefRel :: Position -> Expr a
-    RefAbs :: Position -> Expr a
 
-newtype Position = Position (Int, Int)
-newtype Size = Size (Int, Int)
+data Expr a = ConstInt Int
+    | OperPlus (Expr a) (Expr a)
+    | RefRel Position
+    | RefAbs Position
+    deriving (Functor)
+
+type Expr' = Fix Expr
+type ExprA a = Expr a -> a
+
+instance Show a => Show (Expr a) where
+    show (ConstInt i) = "(ConstInt " ++ show i ++ ")"
+    show (OperPlus i1 i2) = "(OperPlus " ++ show i1 ++ " + " ++ show i2 ++ ")"
+    show (RefRel p) = "(RefRel " ++ show p ++ ")"
+    show (RefAbs p) = "(RefAbs " ++ show p ++ ")"
+
+type Position = (Int, Int)
+type Size = (Int, Int)
 
 type FormulaData = M.Map Position (Expr Int)
-type ViewData = M.Map Position (Either String Int)
+type ViewData = M.Map Position ViewValue
+type ViewValue = (Either String Int)
 
-instance MonadState FormulaData where
-    get :: FormulaData
-    get = undefined
+data Env = Env {
+        formulas :: FormulaData,
+        view :: ViewData,
+        port :: ViewPort
+    }
 
 data ViewPort = ViewPort {
     position :: Position,
     size :: Size
 }
+
+evalExpr :: ExprA (Position -> Env -> ViewValue)
+evalExpr (ConstInt i)     pos _   = Right i
+evalExpr (OperPlus i1 i2) pos env = do
+    f <- i1
+    g <- i2
+    return $ f pos env + g pos env
+evalExpr (RefRel p) pos env = fromMaybe
+    (case M.lookup p (formulas env) of
+        Nothing -> Left ""
+        Just e -> cata evalExpr e p env)
+    (M.lookup p (view env))
+evalExpr (RefAbs p) pos env = fromMaybe
+    (case M.lookup p (formulas env) of
+        Nothing -> Left ""
+        Just e -> cata evalExpr e p env)
+    (M.lookup p (view env))
+
+-- calculate :: Position -> Env -> ViewValue
+-- calculate p env = fromMaybe
+--     (case M.lookup p (formulas env) of
+--         Nothing -> Left ""
+--         Just e -> evalExpr e p env)
+--     (M.lookup p (view env))
+
+evalExpr' = cata evalExpr (Fix (ConstInt 1)) (0, 0) env
+    where
+        env = Env {
+                formulas = M.empty,
+                view = M.empty,
+                port = ViewPort {
+                        position = (0, 0),
+                        size = (0, 0)
+                    }
+            }
 
 eval :: ViewPort -> FormulaData -> ViewData
 eval vp fd = undefined
@@ -36,9 +90,15 @@ render vp vd = undefined
 format :: [[String]] -> IO ()
 format = undefined
 
--- example = do
---     insert (0, 0) 2
---     insert (0, 1) 1
---     insert (0, 2) plus
+computation :: State FormulaData FormulaData
+computation = do
+    x <- get
+    put (M.insert (1 :: Int, 0 :: Int) (ConstInt 1) x)
+    y <- get
+    return y
 
--- insert :: Position -> Expr Int -> FormulaData -> FormulaData
+example :: IO ()
+example = print $ runState computation M.empty
+
+insert :: Position -> Expr Int -> FormulaData -> FormulaData
+insert = undefined
