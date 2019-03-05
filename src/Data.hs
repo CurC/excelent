4fcbdfb6bc570c436d1fe77c5e552b12b5240f7d
CurC <csochinjensem@gmail.com>
2019-03-05 18:59:47 +0100

    Fix recursion scheme use

--- src/Data.hs
+++ src/Data.hs
@@ -1,28 +1,39 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveFunctor #-}
 
 module Data where
 
 import qualified Data.Map as M
-import Control.Monad.State
-import Data.Functor.Foldable (Fix(..), unfix, cata)
+import Data.Functor.Foldable
 import Data.Maybe
 import Debug.Trace
+import GHC.Generics (Generic)
 
 
-data Expr a = ConstInt Int
-    | OperPlus (Expr a) (Expr a)
+data Expr = ConstInt Int
+    | OperPlus Expr Expr
     | RefRel Position
     | RefAbs Position
-    deriving (Functor)
+    deriving (Generic)
 
-type Expr' = Fix Expr
-type ExprA a = Expr a -> a
+data Expr' a = ConstInt' Int
+    | OperPlus' a a
+    | RefRel' Position
+    | RefAbs' Position
+    deriving (Functor, Generic)
 
-instance Show a => Show (Expr a) where
+type Algebra f a = f a -> a
+
+type instance Base Expr = Expr'
+instance Recursive Expr
+instance Corecursive Expr
+
+instance Show Expr where
     show (ConstInt i) = "(ConstInt " ++ show i ++ ")"
     show (OperPlus i1 i2) = "(OperPlus " ++ show i1 ++ " + " ++ show i2 ++ ")"
     show (RefRel p) = "(RefRel " ++ show p ++ ")"
@@ -31,7 +42,7 @@ instance Show a => Show (Expr a) where
 type Position = (Int, Int)
 type Size = (Int, Int)
 
-type FormulaData = M.Map Position (Expr Int)
+type FormulaData = M.Map Position Expr
 type ViewData = M.Map Position ViewValue
 type ViewValue = (Either String Int)
 
@@ -46,21 +57,21 @@ data ViewPort = ViewPort {
     size :: Size
 }
 
-evalExpr :: ExprA (Position -> Env -> ViewValue)
-evalExpr (ConstInt i)     pos _   = Right i
-evalExpr (OperPlus i1 i2) pos env = do
-    f <- i1
-    g <- i2
-    return $ f pos env + g pos env
-evalExpr (RefRel p) pos env = fromMaybe
+evalAlg :: Algebra Expr' (Position -> Env -> ViewValue)
+evalAlg (ConstInt' i)     pos env = Right i
+evalAlg (OperPlus' i1 i2) pos env = do
+    f <- i1 pos env
+    g <- i2 pos env
+    return $ f + g
+evalAlg (RefRel' p) pos env = fromMaybe
     (case M.lookup p (formulas env) of
         Nothing -> Left ""
-        Just e -> cata evalExpr e p env)
+        Just e -> cata evalAlg e p env)
     (M.lookup p (view env))
-evalExpr (RefAbs p) pos env = fromMaybe
+evalAlg (RefAbs' p) pos env = fromMaybe
     (case M.lookup p (formulas env) of
         Nothing -> Left ""
-        Just e -> cata evalExpr e p env)
+        Just e -> cata evalAlg e p env)
     (M.lookup p (view env))
 
 -- calculate :: Position -> Env -> ViewValue
@@ -70,35 +81,52 @@ evalExpr (RefAbs p) pos env = fromMaybe
 --         Just e -> evalExpr e p env)
 --     (M.lookup p (view env))
 
-evalExpr' = cata evalExpr (Fix (ConstInt 1)) (0, 0) env
-    where
-        env = Env {
-                formulas = M.empty,
-                view = M.empty,
-                port = ViewPort {
-                        position = (0, 0),
-                        size = (0, 0)
-                    }
+e1 :: Expr
+e1 = ConstInt 1
+
+e2 :: Expr
+e2 = RefAbs (0, 0)
+
+env1 :: Env
+env1 = Env {
+        formulas = M.empty,
+        view = M.empty,
+        port = ViewPort {
+                position = (0, 0),
+                size = (0, 0)
             }
+    }
+
+env2 :: Env
+env2 = Env {
+        formulas = M.insert (0, 0) (ConstInt 2) M.empty,
+        view = M.empty,
+        port = ViewPort {
+                position = (0, 0),
+                size = (0, 0)
+            }
+    }
+
+evalExpr e env = cata evalAlg e (0, 0) env
 
 eval :: ViewPort -> FormulaData -> ViewData
 eval vp fd = undefined
 
-render :: ViewPort -> ViewData -> [[String]]
-render vp vd = undefined
+-- render :: ViewPort -> ViewData -> [[String]]
+-- render vp vd = undefined
 
-format :: [[String]] -> IO ()
-format = undefined
+-- format :: [[String]] -> IO ()
+-- format = undefined
 
-computation :: State FormulaData FormulaData
-computation = do
-    x <- get
-    put (M.insert (1 :: Int, 0 :: Int) (ConstInt 1) x)
-    y <- get
-    return y
+-- computation :: State FormulaData FormulaData
+-- computation = do
+--     x <- get
+--     put (M.insert (1 :: Int, 0 :: Int) (ConstInt 1) x)
+--     y <- get
+--     return y
 
-example :: IO ()
-example = print $ runState computation M.empty
+-- example :: IO ()
+-- example = print $ runState computation M.empty
 
-insert :: Position -> Expr Int -> FormulaData -> FormulaData
-insert = undefined
+-- insert :: Position -> Expr Int -> FormulaData -> FormulaData
+-- insert = undefined
